cf39d57d329e4642da95ed944acf6ccba16f7b8e
Le Trung Sin <sin.letrung@gmail.com>
2021-07-30 09:09:38 +0700

    s

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-header('Access-Control-Allow-Origin: https://www.etsy.com');
+header('Access-Control-Allow-Origin: *');
 //echo phpinfo();
 echo json_encode($_POST);
 // die;
