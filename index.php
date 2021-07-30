6ff64327beab932bb568b2ccf60cab749a6130bd
Le Trung Sin <sin.letrung@gmail.com>
2021-07-30 09:07:06 +0700

    s

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+header('Access-Control-Allow-Origin: https://esty.com');
 //echo phpinfo();
 echo json_encode($_POST);
 // die;
