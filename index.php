96d761a3a33666dac31a5fa179d6072c87b5e836
Le Trung Sin <sin.letrung@gmail.com>
2021-07-31 09:26:54 +0700

    no message

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-header('Access-Control-Allow-Origin: *');
+//header('Access-Control-Allow-Origin: *');
 //echo phpinfo();
 echo json_encode($_REQUEST['activitiesArray']);
  die;
