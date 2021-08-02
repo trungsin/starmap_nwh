a42427eab1e9dfad62eb8f6427649b195a5a21d6
Le Trung Sin <sin.letrung@gmail.com>
2021-08-02 16:18:02 +0700

    s

--- index.php
+++ index.php
@@ -7,28 +7,28 @@ error_reporting(E_ALL);
 $IDlists = json_encode($_REQUEST['activitiesArray'];
 echo $IDlists;
 die;
-$urlApi = "https://openapi.etsy.com/v2/listings/";
-$curl = curl_init();
+// $urlApi = "https://openapi.etsy.com/v2/listings/";
+// $curl = curl_init();
 
-curl_setopt_array($curl, array(
-    CURLOPT_URL => $urlApi,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'PUT',
-    CURLOPT_POSTFIELDS => 'is_digital=true&type=download'
+// curl_setopt_array($curl, array(
+//     CURLOPT_URL => $urlApi,
+//     CURLOPT_RETURNTRANSFER => true,
+//     CURLOPT_ENCODING => '',
+//     CURLOPT_MAXREDIRS => 10,
+//     CURLOPT_TIMEOUT => 0,
+//     CURLOPT_FOLLOWLOCATION => true,
+//     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//     CURLOPT_CUSTOMREQUEST => 'PUT',
+//     CURLOPT_POSTFIELDS => 'is_digital=true&type=download'
 
-    CURLOPT_HTTPHEADER => array(
-        'Content-Type: application/x-www-form-urlencoded',
-        'x-api-key: 1aa2bb33c44d55eeeeee6fff',
-        'Authorization: Bearer 12345678.jKBPLnOiYt7vpWlsny_lDKqINn4Ny_jwH89hA4IZgggyzqmV_bmQHGJ3HOHH2DmZxOJn5V1qQFnVP9bCn9jnrggCRz'
-    ),
-));
+//     CURLOPT_HTTPHEADER => array(
+//         'Content-Type: application/x-www-form-urlencoded',
+//         'x-api-key: 1aa2bb33c44d55eeeeee6fff',
+//         'Authorization: Bearer 12345678.jKBPLnOiYt7vpWlsny_lDKqINn4Ny_jwH89hA4IZgggyzqmV_bmQHGJ3HOHH2DmZxOJn5V1qQFnVP9bCn9jnrggCRz'
+//     ),
+// ));
 
-$response = curl_exec($curl);
+// $response = curl_exec($curl);
 
-curl_close($curl);
-echo $response;
+// curl_close($curl);
+// echo $response;
