93429dffe8f158b01dabe0bb56e9273b0934778b
Le Trung Sin <sin.letrung@gmail.com>
2021-08-02 16:13:55 +0700

    s

--- index.php
+++ index.php
@@ -4,29 +4,31 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //header('Access-Control-Allow-Origin: *');
 //echo phpinfo();
-echo json_encode($_REQUEST['activitiesArray']);
- die;
-// $curl = curl_init();
+$IDlists = json_encode($_REQUEST['activitiesArray'];
+echo $IDlists.length;
+die;
+$urlApi = "https://openapi.etsy.com/v2/listings/";
+$curl = curl_init();
 
-// curl_setopt_array($curl, array(
-//     CURLOPT_URL => 'https://openapi.etsy.com/v3/application/shops/12345678/listings/192837465',
-//     CURLOPT_RETURNTRANSFER => true,
-//     CURLOPT_ENCODING => '',
-//     CURLOPT_MAXREDIRS => 10,
-//     CURLOPT_TIMEOUT => 0,
-//     CURLOPT_FOLLOWLOCATION => true,
-//     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//     CURLOPT_CUSTOMREQUEST => 'PUT',
-//     CURLOPT_POSTFIELDS => 'is_digital=true&type=download'
+curl_setopt_array($curl, array(
+    CURLOPT_URL => $urlApi,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'PUT',
+    CURLOPT_POSTFIELDS => 'is_digital=true&type=download'
 
-//     CURLOPT_HTTPHEADER => array(
-//         'Content-Type: application/x-www-form-urlencoded',
-//         'x-api-key: 1aa2bb33c44d55eeeeee6fff',
-//         'Authorization: Bearer 12345678.jKBPLnOiYt7vpWlsny_lDKqINn4Ny_jwH89hA4IZgggyzqmV_bmQHGJ3HOHH2DmZxOJn5V1qQFnVP9bCn9jnrggCRz'
-//     ),
-// ));
+    CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/x-www-form-urlencoded',
+        'x-api-key: 1aa2bb33c44d55eeeeee6fff',
+        'Authorization: Bearer 12345678.jKBPLnOiYt7vpWlsny_lDKqINn4Ny_jwH89hA4IZgggyzqmV_bmQHGJ3HOHH2DmZxOJn5V1qQFnVP9bCn9jnrggCRz'
+    ),
+));
 
-// $response = curl_exec($curl);
+$response = curl_exec($curl);
 
-// curl_close($curl);
-// echo $response;
+curl_close($curl);
+echo $response;
