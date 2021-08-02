204c9c83890388a94290dffda00c715c83646350
Le Trung Sin <sin.letrung@gmail.com>
2021-08-02 16:17:15 +0700

    s

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 //header('Access-Control-Allow-Origin: *');
 //echo phpinfo();
 $IDlists = json_encode($_REQUEST['activitiesArray'];
-echo $IDlists.length;
+echo $IDlists;
 die;
 $urlApi = "https://openapi.etsy.com/v2/listings/";
 $curl = curl_init();
