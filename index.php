fd5c531d0da0df9b4b857a2f2ddbc25bca36ce6e
Le Trung Sin <sin.letrung@gmail.com>
2021-08-03 08:25:09 +0700

    c

--- index.php
+++ index.php
@@ -12,27 +12,28 @@ $urlApi = "https://openapi.etsy.com/v2/listings/";
 //$clientId="y8toq4d0ow3zicc5";
 //          exJeyZtXODeekHfX8VRgMQ
 $token = "y3f61lj3isldeeqkf4lhmsd6";
-$url = $urlApi.$IDlists[0] ."?api_key=".$token;
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response_body = curl_exec($ch);
-$response = json_decode($response_body);
+// $url = $urlApi.$IDlists[0] ."?api_key=".$token;
+// $ch = curl_init($url);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// $response_body = curl_exec($ch);
+// $response = json_decode($response_body);
 
-print_r($response->results[0]);
-die;
+// print_r($response->results[0]);
+// die;
 $i=0;
 foreach($IDlists as $IDlist){
-    if($i>5)
-        break;
-    // Make sure you define API_KEY to be your unique, registered key
+    // if($i>5)
+    //     break;
+    // // Make sure you define API_KEY to be your unique, registered key
     //$url = "https://openapi.etsy.com/v2/users/etsystore?api_key=" . API_KEY;
 //    $url = $urlApi.$IDlist ."?api_key=".$token;
     $url = $urlApi.$IDlist."?api_key=".$token;
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response_body = curl_exec($ch);
-    //$responseData = $response_body->results[0];
-    print_r($response_body);echo "<br>";//$responseData->original_creation_tsz."<br>";    
+    $responseData = json_decode($response_body);
+    $listing = responseData->results[0];
+    $listing->original_creation_tsz."<br>";    
 
     //                     var days = moment().diff(moment.unix(responseData.original_creation_tsz), 'days');
     //                     days = days === 0 ? 1 : days;
